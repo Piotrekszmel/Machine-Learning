784b56cb25e7d38cefd501d964ee8aa91383a523
Piotrekszmel <piotr.szmelczynski@gmail.com>
2020-03-22 22:13:25 +0100

    create Regression class and initialize_weights method

--- regression.py
+++ regression.py
@@ -1,4 +1,4 @@
-from __future__ import print_function, division
+from typing import Union
 import numpy as np 
 import math
 
@@ -25,3 +25,25 @@ class l2_regularization:
     
     def grad(self, w):
         return self.alpha * w
+    
+
+class Regression:
+    """Base regression model. Models the relationship between a scalar dependent variable y and the independent 
+    variables X. 
+    Parameters:
+    -----------
+    n_iterations: Union[int, float]
+        The number of training iterations the algorithm will tune the weights for.
+    learning_rate: Union[int, float]
+        The step length that will be used when updating the weights.
+    """
+    def __init__(self, n_iterations: Union[int, float], learning_rate: Union[int, float]): 
+        self.n_iterations = n_iterations
+        self.learning_rate = learning_rate
+    
+    def initialize_weights(self, n_features):
+        """
+        Initialize weights randomly
+        """
+        limit = 1 / math.sqrt(n_features)
+        self.w = np.random.uniform(-limit, limit, (n_features, ))
