0a3882c7e9eb24b3a577206fb7d71f0d8ec3c2b0
Piotrekszmel <piotr.szmelczynski@gmail.com>
2020-03-23 23:42:59 +0100

    create polynomial_features function

--- utils/data_manipulation.py
+++ utils/data_manipulation.py
@@ -8,4 +8,13 @@ def polynomial_features(X, degree):
     def index_combinations():
         combs = [combinations_with_replacement(range(n_features), i) for i in range(0, degree + 1)]
         flat_combs = [item for sublist in combs for item in sublist]
-        return flat_combs]
+        return flat_combs
+    
+    combinations = index_combinations()
+    n_output_feature = len(combinations)
+    X_new = np.empty((n_samples, n_output_feature))
+
+    for i, index_combs in enumerate(combinations):
+        X_new[:, i] = np.prod(X[:, index_combs], axis=1)
+
+    return X_new
