2fda4ef8ab80cd8caca5f533557e20ee10459a0f
Piotrekszmel <piotr.szmelczynski@gmail.com>
2020-03-23 03:05:22 +0100

    create fit and predict method for Regression class

--- regression.py
+++ regression.py
@@ -47,3 +47,24 @@ class Regression:
         """
         limit = 1 / math.sqrt(n_features)
         self.w = np.random.uniform(-limit, limit, (n_features, ))
+
+    def fit(self, X, y):
+        # Insert constant ones for bias weights
+        X = np.insert(X, 0, 1, axis=1)
+        self.training_errors = []
+        self.initialize_weights(n_features=X.shape[1])
+
+        # Do gradient descent for n_iterations
+        for i in range(self.n_iterations):
+            y_pred = X.dot(self.w)
+
+            mse = np.mean(0.5 * (y - y_pred)**2 + self.regularization(self.w))
+            self.training_errors.append(mse)
+
+            grad_w  = -(y - y_pred).dot(X) + self.regularization.grad(self.w)
+            self.w = w - self.learning_rate * grad_w
+    
+    def predict(self, X):
+        X = np.insert(X, 0, 1, axis=1)
+        y_pred = X.dot(self.w)
+        return y_pred
