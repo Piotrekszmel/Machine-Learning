9f08d239747d5f447c1725be2150ff771e25cbc6
Piotrekszmel <piotr.szmelczynski@gmail.com>
2020-03-25 15:07:33 +0100

    create misc.py and data_operation with mse function

---
+++ utils/data_operation.py
@@ -0,0 +1,8 @@
+import numpy as np 
+import math
+
+
+def mean_squared_error(y_true, y_pred):
+    """ Returns the mean squared error between y_true and y_pred """
+    mse = np.mean(np.power((y - y_pred), 2))
+    return mse 
