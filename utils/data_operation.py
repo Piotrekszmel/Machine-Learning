de41e49ff25144f74791008487679a211b182668
Piotrekszmel <piotr.szmelczynski@gmail.com>
2020-03-25 17:27:56 +0100

    add calculate_covariance_matrix and calculate_correlation_matrix functions

--- utils/data_operation.py
+++ utils/data_operation.py
@@ -7,3 +7,25 @@ def mean_squared_error(y_true, y_pred):
     mse = np.mean(np.power((y - y_pred), 2))
     return mse 
     
+
+def calculate_covariance_matrix(X: np.ndarray Y: np.ndarray = None) -> np.ndarray:
+    """ Calculate the covariance matrix for the dataset X """
+    if Y is None:
+        Y = X
+    n_samples = np.shape(X)[0]
+    covariance_matrix = (1 / (n_samples-1)) * (X - X.mean(axis=0)).T.dot(Y - Y.mean(axis=0))
+
+    return np.array(covariance_matrix, dtype=float)
+
+
+def calculate_correlation_matrix(X: np.ndarray, Y: np.ndarray = None) -> np.ndarray:
+    """ Calculate the correlation matrix for the dataset X """
+    if Y is None:
+        Y = X
+    n_samples = np.shape(X)[0]
+    covariance = (1 / n_samples) * (X - X.mean(0)).T.dot(Y - Y.mean(0))
+    std_dev_X = np.expand_dims(calculate_std_dev(X), 1)
+    std_dev_y = np.expand_dims(calculate_std_dev(Y), 1)
+    correlation_matrix = np.divide(covariance, std_dev_X.dot(std_dev_y.T))
+
+    return np.array(correlation_matrix, dtype=float)
