699f38b79e7b45e1517344d25ed81f6f1b97aa15
Piotrekszmel <piotr.szmelczynski@gmail.com>
2020-03-25 12:59:29 +0100

    add shuffle_data function for data_manipulation

--- utils/data_manipulation.py
+++ utils/data_manipulation.py
@@ -2,6 +2,15 @@ from itertools import combinations_with_replacement
 import numpy as np 
 
 
+def shuffle_data(X, y, seed=None):
+    """ Random shuffle of the samples in X and y """
+    if seed:
+        np.random.seed(seed)
+    idx = np.arange(x.shape[0])
+    np.random.shuffle(idx)
+    return X[idx], y[idx]
+
+
 def polynomial_features(X, degree):
     n_samples, n_features = np.shape(X)
 
@@ -25,3 +34,9 @@ def normalize(X, axis=-1, order=2):
     l2 = np.atleast_1d(np.linalg.norm(X, order, axis))
     l2[l2 == 0] = 1
     return X / np.expand_dims(l2, axis)
+
+
+def k_fold_cross_validation_sets(X, y, k, shuffle=True):
+    """ Split the data into k sets of training / test data """
+    if shuffle:
+        X, y = 
