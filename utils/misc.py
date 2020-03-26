561bcfad646bddec381f0f4d6bbd4c76029167ce
Piotrekszmel <piotr.szmelczynski@gmail.com>
2020-03-26 14:05:44 +0100

    add plot_in_2d method for Plot class

--- utils/misc.py
+++ utils/misc.py
@@ -58,3 +58,34 @@ class Plot:
             plt.legend(scatter_plots, scatter_labels, loc=legend["loc"])
         
         plt.show()
+
+    def plot_in_2d(self, X, y=None, title=None, accuracy=None, legend_labels=None):
+        X_transformed = self._transform(X, dim=2)
+        x1 = X_transformed[:, 0]
+        x2 = X_transformed[:, 1]
+        class_distr = []
+
+        y = np.array(y).astype(int)
+
+        colors = [self.cmap(i) for i in np.linspace(0, 1, len(np.unique(y)))]
+
+        for i, l in enumerate(np.unique(y)):
+            _x1 = x1[y == l]
+            _x2 = x2[y == l]
+            _y = y[y == l]
+            class_distr.append(plt.scatter(_x1, _x2, color=colors[i]))
+        
+        #Plot legend
+        if title:
+            if accuracy:
+                perc = 100 * accuracy
+                plt.suptitle(title)
+                plt.title(f"Accuracy: {perc:.1f}%", fontsize=10)
+            else:
+                plt.title(title)
+        
+        #Axis labels
+        plt.xlabel("Principal Component 1")
+        plt.ylabel("Principal Component 2")
+
+        plt.show()
