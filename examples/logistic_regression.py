c0929329bb41ab86b78de6390e1f9f14bfadc205
Piotrekszmel <piotr.szmelczynski@gmail.com>
2020-03-27 23:29:56 +0100

    add gradient, fit and predict methods for LogisticRegression

--- examples/logistic_regression.py
+++ examples/logistic_regression.py
@@ -2,27 +2,33 @@ from sklearn import datasets
 import numpy as np 
 import matplotlib.pyplot as plt 
 
-from MachineLearning.utils.data_manipulation import normalize, make_diagonal, train_test_split
-from MachineLearning.deep_learning.activation_functions import Sigmoid
+from MachineLearning.utils.data_manipulation import train_test_split, normalize
+from MachineLearning.utils.data_operation import accuracy_score
+from MachineLearning.utils.misc import Plot
+from MachineLearning.LogisticRegression import LogisticRegression
 
 
-class LogisticRegression:
-    """ 
-    Logistic Regression classifier.
-    Parameters:
-    -----------
-    learning_rate: float
-        The step length that will be taken when following the negative gradient during
-        training.
-    """
-    def __init__(self, learning_rate = 0.1):
-        self.param = param
-        self.learning_rate = learning_rate
-        self.sigmoid = Sigmoid()
+def main():
+    data = datasets.load_iris()
+    X = normalize(data.data[data.target != 0])
+    y = data.target[data.target != 0]
+
+    y[y==1] = 0
+    y[y==2] = 1
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, seed=1)
+
+    clf = LogisticRegression()
+    clf.fit(X_train, y_train)
+    y_pred = clf.predict(X_test)
+
+    accuracy = accuracy_score(y_test, y_pred)
+    print("Accuracy: ", accuracy)
     
-    def _initialize_parameters(self, X):
-        n_features = np.shape(X)[1]
-        # Initialize parameters between [-1/sqrt(N), 1/sqrt(N)]
-        limit = 1 / math.sqrt(n_features)
-        self.param = np.random.uniform(-limit, limit, (n_features,))
-        
+    Plot().plot_in_2d(X_test, y_pred, title="Logistic Regression", accuracy=accuracy)
+
+
+if __name__ == "__main__":
+    main()
+
+
