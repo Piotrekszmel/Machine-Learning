3ad3e3688589ef9bca7e5d2977ad7fcbcdaa0ccf
Piotrekszmel <piotr.szmelczynski@gmail.com>
2020-03-28 19:39:12 +0100

    add _classify and predict method to NaiveBayes

--- naive_bayes.py
+++ naive_bayes.py
@@ -7,7 +7,7 @@ from MachineLearning.utils.data_operation import accuracy_score
 
 class NaiveBayes:
     """The Gaussian Naive Bayes classifier. """
-    def fit(self, X, y):
+    def fit(self, X: np.ndarray, y: np.ndarray) -> None:
         self.X = X
         self.y = y
         self.classes = np.unique(y)
@@ -21,7 +21,7 @@ class NaiveBayes:
                  parameters = {"mean": col.mean(), "var": col.var()}
                  self.parameters[i].append(parameters)
 
-    def _calculate_likelihood(self, mean, x):
+    def _calculate_likelihood(self, mean, var, x):
         """ Gaussian likelihood of the data x given mean and var """
          # Added in denominator to prevent division by zero
         eps = 1e-4
@@ -36,3 +36,33 @@ class NaiveBayes:
         """ 
         frequency = np.mean(self.y == c)
         return frequency
+    
+    def _classify(self, sample):
+        """ 
+        Classification using Bayes Rule P(Y|X) = P(X|Y)*P(Y)/P(X),
+            or Posterior = Likelihood * Prior / Scaling Factor
+        P(Y|X) - The posterior is the probability that sample x is of class y given the
+                 feature values of x being distributed according to distribution of y and the prior.
+        P(X|Y) - Likelihood of data X given class distribution Y.
+                 Gaussian distribution (given by _calculate_likelihood)
+        P(Y)   - Prior (given by _calculate_prior)
+        P(X)   - Scales the posterior to make it a proper probability distribution.
+                 This term is ignored in this implementation since it doesn't affect
+                 which class distribution the sample is most likely to belong to.
+        Classifies the sample as the class that results in the largest P(Y|X) (posterior)
+        """
+        posteriors = []
+
+        for i, c in enumerate(self.classes):
+            # Initialize posterior as prior
+            posterior = self._calculate_prior(c)
+            for feature_value, params in zip(sample, self.parameters[i]):
+                likelihood = self._calculate_likelihood(params["mean"], params["var"], feature_value)
+                posterior *= likelihood
+            posteriors.append(posterior)
+        return self.classes(np.argmax(posteriors))
+    
+    def predict(self, X: np.ndarray):
+        """ Predict the class labels of the samples in X """
+        y_pred = [self._classify(sample) for sample in X]
+        return y_pred
