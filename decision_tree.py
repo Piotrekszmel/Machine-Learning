6c94026ba955f84124c6864e9d38555250a7cc9f
Piotrekszmel <piotr.szmelczynski@gmail.com>
2020-03-30 15:04:24 +0200

    create fit and _build_tree method for DecisionTree

--- decision_tree.py
+++ decision_tree.py
@@ -28,3 +28,88 @@ class DecisionNode:
         self.value = value
         self.true_branch = true_branch
         self.false_branch = false_branch
+
+
+class DecisionTree:
+    """
+    Super class of RegressionTree and ClassificationTree.
+    Parameters:
+    -----------
+    min_samples_split: int
+        The minimum number of samples needed to make a split when building a tree.
+    min_impurity: float
+        The minimum impurity required to split the tree further.
+    max_depth: int
+        The maximum depth of a tree.
+    loss: function
+        Loss function that is used for Gradient Boosting models to calculate impurity.
+    """
+    def __init__(self, min_samples_split=2, min_impurity=1e-7, 
+                max_depth=float("inf"), loss=None):
+        self.root = None
+        self.min_samples_split = min_samples_split
+        self.min_impurity = min_impurity
+        self.max_depth = max_depth
+        self._impurity_calculation = None
+        self._leaf_value_calculation = None
+        self.one_dim = None
+        self.loss = loss
+    
+    def fit(self, X, y, loss=None):
+        """ Build decision tree """
+        self.one_dim = len(np.shape(y)) == 1
+        self.root = self._build_tree(X, y)
+        self.loss = None
+    
+    def _build_tree(self, X, y, current_depth):
+        """ 
+        Recursive method which builds out the decision tree and splits X and respective y
+        on the feature of X which (based on impurity) best separates the data
+        """
+
+        largest_impurity = 0
+        best_criteria = None
+        best_sets = None
+
+        if len(np.shape(y)) == 1:
+            y = np.expand_dims(y, axis=1)
+        
+        Xy = np.concatenate((X, y), axis=1)
+
+        n_samples, n_features = np.shape(X)
+
+        if n_samples >= self.min_samples_split and current_depth <= self.max_depth:
+            for feature_i in range(n_features):
+                feature_values = np.expand_dims(X[:, feature_i], axis=1)
+                unique_values = np.unique(feature_values)
+
+                for threshold in unique_values:
+                    Xy1, Xy2 = divide_on_feature(Xym feature_i, threshold)
+
+                    if len(Xy1) > 0 and len(Xy2) > 0:
+                        y1 = Xy1[:, n_features:]
+                        y2 = Xy2[:, n_features:]
+
+                        impurity = self._impurity_calculation(y, y1, y2)
+
+                        if impurity > largest_impurity:
+                            largest_impurity = impurity
+                            best_criteria = {"feature_i": feature_i, "threshold": threshold}
+                            best_sets = {
+                                "leftX": Xy1[:, :n_features],   # X of left subtree
+                                "lefty": Xy1[:, n_features:],   # y of left subtree
+                                "rightX": Xy2[:, :n_features],  # X of right subtree
+                                "righty": Xy2[:, n_features:]   # y of right subtree
+                                }
+
+        if largest_impurity > self.min_impurity:
+            # Build subtrees for the right and left branches
+            true_branch = self._build_tree(best_sets["leftX"], best_sets["lefty"], current_depth + 1)
+            false_branch = self._build_tree(best_sets["rightX"], best_sets["righty"], current_depth + 1)
+            return DecisionNode(feature_i=best_criteria["feature_i"], threshold=best_criteria[
+                                "threshold"], true_branch=true_branch, false_branch=false_branch)
+
+        # We're at leaf => determine value
+        leaf_value = self._leaf_value_calculation(y)
+
+        return DecisionNode(value=leaf_value)
