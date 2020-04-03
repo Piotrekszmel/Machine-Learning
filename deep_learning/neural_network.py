d832be1c65d8185b27046e8bddbee9b87488931e
Piotrekszmel <piotr.szmelczynski@gmail.com>
2020-04-03 13:35:08 +0200

    create fit method for NeuralNetwork

--- deep_learning/neural_network.py
+++ deep_learning/neural_network.py
@@ -67,5 +67,30 @@ class NeuralNetwork:
         loss_gradient = self.loss_function.gradient(y, y_pred)
         #Backpropagate. Update weights
         self._backward_pass(loss_gradient)
+
+        return loss, acc
     
+    def test_on_batch(self, X, y):
+        """ Evaluates the model over a single batch """
+        y_pred self._forward_pass(X, training=False)
+        loss = np.mean(self.loss_function.loss(y, y_pred))
+        acc = self.loss_function.acc(y, y_pred)
+
+        return loss, acc
     
+    def fit(self, X, y, n_epochs, batch_size):
+        """ Trains the model for a fixed number of epochs """
+        for _ in self.progressbar(range(n_epochs)):
+
+            batch_error = []
+            for X_batch, y_batch in batch_iterator(x, y, batch_size=batch_size):
+                loss, _ = self.train_on_batch(X, y)
+                batch_error.append(loss)
+        
+        self.errors["training"].append(np.mean(batch_error))
+
+        if self.val_set is not None:
+            val_loss, _ = self.test_on_batch(self.val_set["X"], self.val_set["y"])
+            self.errors["validations"].append(np.mean(val_loss))
+        
+        return self.errors["training"], self.errors["validation"]
