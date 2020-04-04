e61fdf7c3a47ea75065643a679388f3537a3be46
Piotrekszmel <piotr.szmelczynski@gmail.com>
2020-04-04 16:01:32 +0200

    add CrossEntropy class

--- deep_learning/loss_functions.py
+++ deep_learning/loss_functions.py
@@ -3,3 +3,32 @@ import numpy as np
 from MachineLearning.deep_learning.activation_functions import Sigmoid
 from MachineLearning.utils.data_operation import accuracy_score
 
+
+class Loss:
+    def loss(self, y, y_pred):
+        return NotImplementedError()
+    
+    def gradient(self, y, y_pred):
+        raise NotImplementedError()
+
+    def acc(self, y, y_pred):
+        return 0
+
+
+class SquareLoss(Loss):
+    def __init__(self):
+        pass
+
+    def loss(self, y, y_pred):
+        return 0.5 * np.power((y - y_pred), 2)
+    
+    def gradient(self, y, y_pred):
+        return -(y - y_pred)
+
+class CrossEntropy(Loss):
+    def __init__(self):
+        pass
+    
+    def loss(self, y, p):
+        p = np.clip(p, 1e-15, 1 - 1e-15)
+        return -(y * np.log(p) + (1 - y) * np.log(1 - p))
