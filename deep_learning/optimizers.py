27fbd78a4b0d042ca9a5ab11f2c426b32128e504
Piotrekszmel <piotr.szmelczynski@gmail.com>
2020-04-17 19:18:32 +0200

    fix bug with return in SGD optimizer. Add parameters types and comments to SGD class

--- deep_learning/optimizers.py
+++ deep_learning/optimizers.py
@@ -3,14 +3,15 @@ from MachineLearning.utils.data_manipulation import normalize, make_diagonal
 
 
 class SGD:
-    def __init__(self, lr=0.01, momentum=0.):
+    def __init__(self, lr: float = 0.01, momentum: float = 0.):
         self.lr = lr 
         self.momentum = momentum
         self.v = None
     
-    def update(self, w, grad_w):
+    def update(self, w: np.ndarray, grad_w: np.ndarray) -> np.ndarray:
         if self.v is None:
             self.v = np.zeros(np.shape(w))
-        
+        # Optional use of momentum if set
         self.v = self.momentum * self.v + (1 - self.momentum) * grad_w
-        return w = self.lr * self.lr
+        # Move against the gradient to minimize loss
+        return w - self.lr * self.v
